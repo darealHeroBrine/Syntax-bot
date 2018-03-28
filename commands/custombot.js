74e2c659a1b85cda16fe6f6f72764b3805712e8a
darealHeroBrine <33338073+darealHeroBrine@users.noreply.github.com>
2018-03-27 19:18:25 -0700

    Create custombot.js

---
+++ commands/custombot.js
@@ -0,0 +1,24 @@
+const settings = require('../settings.json');
+exports.run = (client, message, params) => {
+  DMchannel.sendMessage('Here is the bot request forum.'),
+  DMchannel.send('https://goo.gl/forms/f1l0lu94j78dgpvo1', {
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.jpg'
+      }
+   }
+})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['cb', 'custom', 'cbot', 'customb'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'custombot',
+  description: 'sends you the custom bot request forum.',
+  usage: 'custombot'
+};
