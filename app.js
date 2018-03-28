85fe28b6ce52bbbcfbe28b00598b081935c39e1c
darealHeroBrine <33338073+darealHeroBrine@users.noreply.github.com>
2018-03-27 22:00:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ client.elevation = message => {
   if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
   const support_role = message.guild.roles.find('name', settings.supportrolename);
   if (support_role && message.member.roles.has(support_role.id)) permlvl = 4;
+  if (message.author.id === process.env.BOT_SUPPORT) permlvl = 7;
   if (message.author.id === process.env.OWNER_ID) permlvl = 10;
   return permlvl;
 };
