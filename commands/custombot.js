087f5d0e8f148c313aa395a0c25471935f08a44a
darealHeroBrine <33338073+darealHeroBrine@users.noreply.github.com>
2018-03-28 20:27:49 -0700

    Update custombot.js

--- commands/custombot.js
+++ commands/custombot.js
@@ -1,7 +1,7 @@
 const settings = require('../settings.json');
 exports.run = (client, message, params) => {
-  channel.send.DMchannel('Here is the bot request forum.'),
-  channel.send.DMchannel('https://goo.gl/forms/f1l0lu94j78dgpvo1')
+  message.dmchannel.send('Here is the bot request forum.'),
+  message.dmchannel.send('https://goo.gl/forms/f1l0lu94j78dgpvo1')
 };
 
 exports.conf = {
