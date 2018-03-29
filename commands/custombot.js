60042f20541d9989292c873a8c2ad7ebfb477120
darealHeroBrine <33338073+darealHeroBrine@users.noreply.github.com>
2018-03-28 20:16:37 -0700

    Update custombot.js

--- commands/custombot.js
+++ commands/custombot.js
@@ -1,13 +1,7 @@
 const settings = require('../settings.json');
 exports.run = (client, message, params) => {
   channel.DMchannel.send('Here is the bot request forum.'),
-  channel.DMchannel.send('https://goo.gl/forms/f1l0lu94j78dgpvo1', {
-  embed: {
-    thumbnail: {
-         url: 'attachment://file.jpg'
-      }
-   }
-})
+  channel.DMchannel.send('https://goo.gl/forms/f1l0lu94j78dgpvo1')
 };
 
 exports.conf = {
